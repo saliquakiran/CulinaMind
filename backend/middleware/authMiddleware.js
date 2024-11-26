d6bf842b38c5bc3537946d134453a97ff2027cfa
saliquakiran <saliqua@uni.minerva.edu>
2024-11-25 23:54:23 -0800

    Auth Middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "secretKeyForJWT";
+
+module.exports = (req, res, next) => {
+  const token = req.header("Authorization");
+
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
